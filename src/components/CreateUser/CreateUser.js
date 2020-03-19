7974e401e5845ecffac0ca58ce45787cf6ad4219
Mothi Sampath <Mothi_Sampath@Trimble.com>
2020-03-19 15:13:57 +0530

    create user commit

--- src/components/CreateUser/CreateUser.js
+++ src/components/CreateUser/CreateUser.js
@@ -4,87 +4,81 @@ import axios from 'axios'
 import App from '../../App';
 
 
-
-
-
 export default class CreateUser extends React.Component {
     constructor(props) {
-      super(props);
-      this.state = {
-        username: '',
-        email : '',
-        password : null
-      };
+        super(props);
+        this.state = {
+            username: '',
+            email: '',
+            password: null
+        };
     }
 
-    closePopusOnClick(){
+    closePopusOnClick() {
         this.popup.current.leafletElement.options.leaflet.map.closePopup();
     }
 
     mySubmitHandler = (event) => {
-        const self =this;
-      event.preventDefault();
-      let username = this.state.username;
-      if (Number(username)) {
-        alert("user name should not be numbers");
-      }
+        const self = this;
+        event.preventDefault();
+        let username = this.state.username;
+        if (Number(username)) {
+            alert("user name should not be numbers");
+        }
+        axios.post('http://localhost:8080/users/saveUser', {
+            userName: this.state.username,
+            password: this.state.password,
+            email: this.state.email
+        })
+            .then(function (response) {
+                console.log(response);
+                alert('Success')
+                self.close();
 
-      
+            })
+            .catch(function (error) {
+                console.log(error);
 
-      axios.post('http://localhost:8080/users/saveUser', {
-        userName: this.state.username,
-        password: this.state.password,
-        email: this.state.email
-      })
-      .then(function (response) {
-        console.log(response);
-        alert('Success')
-        self.close();
-       
-      })
-      .catch(function (error) {
-        console.log(error);
-        
-      });
-
-      
+            });
     }
+
     myChangeHandler = (event) => {
-      let nam = event.target.name;
-      let val = event.target.value;
-      this.setState({[nam]: val});
+        let nam = event.target.name;
+        let val = event.target.value;
+        this.setState({[nam]: val});
     }
-    render() {
-      return (
-        <form onSubmit={this.mySubmitHandler  }>
-        <h1>Save User</h1>
-        <p>UserName:</p>
-        <input
-          type='text'
-          name='username'
-          onChange={this.myChangeHandler}
-        />
-         <p>Email:</p>
-        <input
-          type='text'
-          name='email'
-          onChange={this.myChangeHandler}
-        />
-        <p>Password:</p>
-        <input
-          type='text'
-          name='password'
-          onChange={this.myChangeHandler}
-        />
-        <br/>
-        <br/>
-        <input type='submit' />
-        </form>
 
-        
-      );
+    render() {
+        return (
+            <form onSubmit={this.mySubmitHandler}>
+                <h1>Save User</h1>
+                <p>UserName:</p>
+                <input
+                    type='text'
+                    name='username'
+                    onChange={this.myChangeHandler}
+                />
+                <p>Email:</p>
+                <input
+                    type='text'
+                    name='email'
+                    onChange={this.myChangeHandler}
+                />
+                <p>Password:</p>
+                <input
+                    type='text'
+                    name='password'
+                    onChange={this.myChangeHandler}
+                />
+                <br/>
+                <br/>
+                <input type='submit'/>
+            </form>
+
+
+        );
     }
-  }
-  
-  ReactDOM.render(<CreateUser />, document.getElementById('root'))
+}
+
+ReactDOM.render(<CreateUser/>, document.getElementById('root'))
 
